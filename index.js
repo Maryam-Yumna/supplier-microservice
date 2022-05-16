a30ebadc3c4a75e710547c3f10a8e222310ef72e
Maryam-Yumna <maryamyumna@gmail.com>
2022-05-17 03:14:11 +0530

    deploy

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const { urlencoded } = require("express");
 
 let app = express();
 
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8080;
 
 app.use(urlencoded({ extended: true }));
 app.use(cors());
